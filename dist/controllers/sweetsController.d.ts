a5df10a18cc0f467826b379562d0de48fa1d2529
unknown <pranavkulkarni33917@gmail.com>
2025-09-20 22:06:53 +0530

    vercel-deployment

--- dist/controllers/sweetsController.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import { AuthRequest } from "../middleware/auth";
-export declare function addSweet(req: AuthRequest, res: Response): Promise<void>;
-export declare function listSweets(req: Request, res: Response): Promise<void>;
-export declare function searchSweets(req: Request, res: Response): Promise<void>;
-export declare function updateSweet(req: AuthRequest, res: Response): Promise<void>;
-export declare function deleteSweet(req: AuthRequest, res: Response): Promise<void>;
-export declare function purchaseSweet(req: AuthRequest, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-export declare function restockSweet(req: AuthRequest, res: Response): Promise<void>;
--- dist/utils/jwt.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function generateToken(userId: number, role: string): string;
-export declare function verifyToken(token: string): {
-    userId: number;
-    role: string;
-};
