baa018e4a9c53fe0d18b7070dff236e1f4dda247
unknown <pranavkulkarni33917@gmail.com>
2025-09-21 22:05:40 +0530

    vercel hosting

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ import app from "./app";
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+
+// This part runs the server only when you are NOT on Vercel
+if (process.env.VERCEL !== '1') {
+    app.listen(PORT, () => {
+        console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+}
+
+// This line is essential for Vercel
+export default app;
